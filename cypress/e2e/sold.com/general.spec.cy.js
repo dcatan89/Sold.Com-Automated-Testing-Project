f4af04a304587fe45136b9d5e304298524140c36
Daniel Catan <danieljcatan@gmail.com>
2022-07-24 07:24:18 -0700

    working on first spec test, will set up server and node next

--- cypress/e2e/sold.com/general.spec.cy.js
+++ cypress/e2e/sold.com/general.spec.cy.js
@@ -1,5 +1,14 @@
-describe('Opens the TodoMVC', () => {
-  it('Visits the todo app', () => {
-    cy.visit('/')
+describe('Opening the TodoMVC app to check general specs', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Opens initial TodoMVC app', () => {
+    cy.get('[data-reactid=".0.0"]').should('not.be.visible')
+    cy.get('[data-reactid=".0.2"]').should('not.be.visible')
+  })
+
+  it('Opens initial display to see if it is focused on input field', () => {
+    cy.get('[data-reactid=".0.0.1"]')
   })
 })
