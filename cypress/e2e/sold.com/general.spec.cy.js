e1ab6d68f8d00e85deeaf3a97cd30ced9df790d9
Daniel Catan <danieljcatan@gmail.com>
2022-07-24 10:23:10 -0700

    test 1-4 working fine

--- cypress/e2e/sold.com/general.spec.cy.js
+++ cypress/e2e/sold.com/general.spec.cy.js
@@ -1,14 +1,20 @@
 describe('Opening the TodoMVC app to check general specs', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
   })
 
-  it('Opens initial TodoMVC app', () => {
-    cy.get('[data-reactid=".0.0"]').should('not.be.visible')
-    cy.get('[data-reactid=".0.2"]').should('not.be.visible')
+  it('Opens initial TodoMVC app to see if main and footer is hidden while input is focused', () => {
+    cy.get('[data-id="main"]').should('not.be.visible')
+    cy.get('[data-id="footer"]').should('not.be.visible')
+    cy.get('[data-reactid=".0.0.1"]').should('have.focus')
   })
 
-  it('Opens initial display to see if it is focused on input field', () => {
-    cy.get('[data-reactid=".0.0.1"]')
+  it('Add todo and enter to clear text field, footer and header should be visible', () => {
+    cy.get('[data-reactid=".0.0.1"]').type('Hello, world{enter}')
+    cy.get('[data-reactid=".0.0.1"]').should('be.empty')
+
+    cy.get('[data-reactid=".0.0"').should('be.visible')
+    cy.get('[data-reactid=".0.2"').should('be.visible')
   })
+
 })
