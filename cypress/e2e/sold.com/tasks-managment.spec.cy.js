48951a617d3a0df556ceecf56b07d6a5ae027b1a
Daniel Catan <danieljcatan@gmail.com>
2022-07-24 13:08:26 -0700

    Task Management tests 1-2 working correctly

--- cypress/e2e/sold.com/tasks-managment.spec.cy.js
+++ cypress/e2e/sold.com/tasks-managment.spec.cy.js
@@ -1,5 +1,18 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Task-Manangement Specs', () => {
+  beforeEach(() => {
+    cy.visit('/')
+    cy.get('[data-reactid=".0.0.1"]').type('Hello, world{enter}')
+    cy.get('[data-reactid=".0.0.1"]').type('Hello Back{enter}')
   })
+
+  it('Test 1: Clicks Mark all as completed ', () => {
+    cy.get('[data-reactid=".0.1.1"]').click()
+    cy.get('li').should('have.class', 'completed')
+  })
+
+  it('Test 2: Toggle Mark removes li completed class', () => {
+    cy.get('[data-reactid=".0.1.1"]').click().click()
+    cy.get('li').should('not.have.class', 'completed')
+  })
+
 })
